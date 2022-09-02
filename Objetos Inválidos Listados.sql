01d7de3c9c23b73a4bad3355e6797b29ef484246
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-02 15:25:44 -0300

    Update Objetos Inválidos Listados.sql

--- Objetos Inválidos Listados.sql
+++ Objetos Inválidos Listados.sql
@@ -1,19 +1,6 @@
 select owner "Owner",
-
 object_name "Objeto",
-
 status "Estado"
-
 from dba_objects
-
 where lower(status) != 'valid'
-
---and lower(owner) = '{nome_do_owner}'
-
--- a linha acima pode ser eliminada para
-
--- que todos os objetos inválidos sejam
-
--- listados, independente do owner
-
 order by owner, object_name;
