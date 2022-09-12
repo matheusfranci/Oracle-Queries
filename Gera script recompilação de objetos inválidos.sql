93738ee1015aa89744a35dc70edc43758bc12bdd
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-12 10:56:30 -0300

    Create Gera script recompilação de objetos inválidos.sql

---
+++ Gera script recompilação de objetos inválidos.sql
@@ -0,0 +1,5 @@
+Select 'Alter '||decode(object_type,'PACKAGE BODY','PACKAGE','UNDEFINED','SNAPSHOT',OBJECT_TYPE)||' '||
+       Owner || '.' || decode(substr(Object_name, 1, 4), '_ALL', '"' || object_name || '"', object_name) ||' '||
+       decode(object_type,'PACKAGE BODY','COMPILE BODY','COMPILE')||';'
+From   Dba_Objects
+where  Status <> 'VALID';
