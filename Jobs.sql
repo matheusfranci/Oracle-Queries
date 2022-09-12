983fcfff484ed5d99ae94069c65e220d6265be8d
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-12 07:57:22 -0300

    Update Jobs.sql

--- Jobs.sql
+++ Jobs.sql
@@ -1,3 +1,11 @@
+** VERIFICANDO UM JOB **
+select job_name, owner, enabled, state from dba_scheduler_jobs;
+
+** TODAS AS INFORMAÇÕES DE UM JOB ESPECÍFICO **
 select * from DBA_SCHEDULER_JOB_RUN_DETAILS where JOB_NAME = 'nome do job';
 
-SELECT JOB_NAME, STATE FROM DBA_SCHEDULER_JOBS;
+** HABILITANDO UM JOB **
+execute dbms_scheduler.enable('owner.job');
+
+** DESABILITANDO UM JOB **
+execute dbms_scheduler.disable('owner.job');
