5e32411838cf9a31f486a69e49ca86a4f7446af6
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-12 08:04:18 -0300

    Update Jobs.sql

--- Jobs.sql
+++ Jobs.sql
@@ -1,5 +1,6 @@
 ** VERIFICANDO UM JOB **
 select job_name, owner, enabled, state from dba_scheduler_jobs;
+select last_run_duration, next_run_date, run_count, failure_count, schedule_name, owner, job_name, state from dba_scheduler_jobs;
 
 ** TODAS AS INFORMAÇÕES DE UM JOB ESPECÍFICO **
 select * from DBA_SCHEDULER_JOB_RUN_DETAILS where JOB_NAME = 'nome do job';
