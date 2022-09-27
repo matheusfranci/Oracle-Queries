e867993246290aa718ecb6542ba2791e4020ab37
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-27 17:46:02 -0300

    Create Characterset.sql

---
+++ Characterset.sql
@@ -0,0 +1,5 @@
+-- consulta o atual
+select value from nls_database_parameters where parameter = 'NLS_CHARACTERSET';
+
+-- substitui o atual pelo WE8ISO8859P1
+ALTER DATABASE CHARACTER SET INTERNAL_USE WE8ISO8859P1;
