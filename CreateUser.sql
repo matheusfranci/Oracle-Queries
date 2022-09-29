f926d4a50953220be58025bdafb69a5018e31a7e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-29 10:40:46 -0300

    Create CreateUser.sql

---
+++ CreateUser.sql
@@ -0,0 +1,10 @@
+--Usuário exemplo criado, segue comando de criação:
+
+CREATE USER exemlo IDENTIFIED BY "null"
+PASSWORD EXPIRE ;
+
+Seguem os grants;
+
+GRANT CONNECT TO exemplo;
+
+grant create session, select any table, select any dictionary to exemplo;
