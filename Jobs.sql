cffce696e0e8bb96f540d2bce6f5dc375e7b0f1e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-09-30 10:04:04 -0300

    Update Jobs.sql

--- Jobs.sql
+++ Jobs.sql
@@ -5,13 +5,13 @@ select * from dba_jobs;
 select * from dba_jobs where job between '504' AND '509';
 select JOB, SCHEMA_USER, LAST_DATE, LAST_SEC, BROKEN, FAILURES, NEXT_SEC from dba_jobs where job between '504' AND '509';
 
-** TODAS AS INFORMAÇÕES DE UM JOB ESPECÍFICO **
+-- TODAS AS INFORMAÇÕES DE UM JOB ESPECÍFICO 
 select * from DBA_SCHEDULER_JOB_RUN_DETAILS where JOB_NAME = 'nome do job';
 
-** HABILITANDO UM JOB **
+-- HABILITANDO UM JOB 
 execute dbms_scheduler.enable('owner.job');
 exec dbms_job.broken(1,FALSE);
 
-** DESABILITANDO UM JOB **
+-- DESABILITANDO UM JOB
 execute dbms_scheduler.disable('owner.job');
 exec dbms_job.broken(1,TRUE);
