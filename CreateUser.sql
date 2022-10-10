04fa82f2c7d0413245eefca99afd302124c76aa2
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-10-10 11:39:39 -0300

    Update CreateUser.sql

--- CreateUser.sql
+++ CreateUser.sql
@@ -11,3 +11,13 @@ GRANT CONNECT TO exemplo;
 grant create session, select any table, select any dictionary to exemplo;
 
 grant dba to matheusorion with admin option;
+
+
+--Multitenant
+grant create session to matheusorion container=all;
+
+grant dba to matheusorion container=all;
+
+grant select any dictionary to matheusorion container=all;
+
+alter user matheusorion set container_data=all container=current;
