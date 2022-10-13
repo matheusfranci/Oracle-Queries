3b614b2c9a6583c36d6e92128dec28b158e15126
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-10-13 00:59:53 -0300

    Update rman queries.sql

--- rman queries.sql
+++ rman queries.sql
@@ -6,3 +6,26 @@ report obsolete;
 
 -- Exclusão
 delete obsolete;
+
+-- Verificar schemas do cdb e pdb
+report schema
+
+-- Backup de tudo(pdbs e cdbs) com exceção dos arquivelogs
+BACKUP DATABASE;
+BACKUP FULL DATABASE;
+
+-- Backup de tudo(pdbs e cdbs) com arquivelogs
+BACKUP DATABASE PLUS ARCHIVELOG;
+BACKUP FULL DATABASE PLUS ARCHIVELOG;
+
+--Backups apenas dos archivelogs
+BACKUP ARCHIVELOG ALL;
+
+--Backup apenas do cdbroot
+BACKUP DATABASE root;
+BACKUP DATABASE "CDB$ROOT";
+BACKUP PLUGGABLE DATABASE "CDB$ROOT";
+
+--Backup apenas do pdb
+BACKUP PLUGGABLE DATABASE ORCLPDB;
+BACKUP PLUGGABLE DATABASE ORCLPDB, PDBTESTDBA;
