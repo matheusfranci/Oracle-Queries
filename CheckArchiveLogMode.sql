57caf53904ea218c9ac7951eff4b3df3c9f2614c
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-10-13 16:24:09 -0300

    Update CheckArchiveLogMode.sql

--- CheckArchiveLogMode.sql
+++ CheckArchiveLogMode.sql
@@ -1,1 +1,19 @@
+--verificar o archivemodelog
 select log_mode from v$database;
+
+-- derrube a instância
+shuwtdown IMMEDIATE;
+
+-- suba com MOUNT
+startup MOUNT;
+
+-- altere o modo de ARCHIVELOG
+alter database ARCHIVELOG;
+
+-- abra o banco
+alter database open;
+
+--verificar o archivemodelog
+select log_mode from v$database;
+
+archive log list
