7c67a95b96a48afd1b8cb2a8cf5596a0bb1e1384
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-01 19:56:37 -0300

    Update OracleEM.sql

--- OracleEM.sql
+++ OracleEM.sql
@@ -1,8 +1,11 @@
--- Verifique se há porta https configurada
+-- Verifique se há porta HTTPS configurada
 select dbms_xdb_config.getHttpsPort() from dual;
 
+-- Verifique se há porta HTTP configurada
+select dbms_xdb_config.getHttpPort() from dual;
+
 -- Caso tenha use-a ou mude através dessa procedure
-exec dbms_xdb_config.sethttpsport(porta aqui)
+exec dbms_xdb_config.sethttpsport(5500)
 
 -- Depois verifique se mostra no listener
 lsnrctl status
