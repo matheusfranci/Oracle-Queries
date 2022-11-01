ffd834d2ad0243a2e677d5314bf65264663e4848
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-01 11:45:38 -0300

    Update Listar schemas.sql

--- Listar schemas.sql
+++ Listar schemas.sql
@@ -1,4 +1,11 @@
+-- Listando schemas
 select distinct owner schema_name
 from dba_segments
 where owner in(select username from dba_users where default_tablespace not in ('SYSTEM','SYSAUX'))
 order by owner;
+
+-- Mudando de schema
+ALTER SESSION SET CURRENT_SCHEMA=ZEUSMANAGER;
+
+-- Verificando schema atual
+select sys_context( 'userenv', 'current_schema' ) from dual;
