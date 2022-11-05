10ddabf3b5fd543bda4ca77be483b76e29e460a4
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-05 18:36:26 -0300

    Update CheckASM.sql

--- CheckASM.sql
+++ CheckASM.sql
@@ -1,2 +1,6 @@
 SELECT name, type, total_mb, free_mb/1024 free_gb, required_mirror_free_mb, STATE,
 usable_file_mb FROM V$ASM_DISKGROUP;
+
+-- SCP dentro do asm
+-- Precisa ser feito dentro do usuário GRID
+ASMCMD> cp /u01/app/oracle/rm/datapump/EXPDP_RM.dmp +DATA/dump/EXPDP_RM.dmp
