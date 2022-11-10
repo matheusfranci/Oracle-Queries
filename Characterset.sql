8f40954fe4e6cd8b0ed3333af92f4c6613c3cc8c
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-10 13:34:10 -0300

    Update Characterset.sql

--- Characterset.sql
+++ Characterset.sql
@@ -8,3 +8,7 @@ select value from nls_database_parameters where parameter = 'NLS_CHARACTERSET';
 
 -- substitui o atual pelo WE8ISO8859P1
 ALTER DATABASE CHARACTER SET INTERNAL_USE WE8ISO8859P1;
+
+-- Tirandoo do modo restrito
+alter system disable restricted session;
+ 
