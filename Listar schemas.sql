b7b89e791a155743f9d1854e7faafb23f668b517
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-22 16:18:50 -0300

    Update Listar schemas.sql

--- Listar schemas.sql
+++ Listar schemas.sql
@@ -9,3 +9,15 @@ ALTER SESSION SET CURRENT_SCHEMA=ZEUSMANAGER;
 
 -- Verificando schema atual
 select sys_context( 'userenv', 'current_schema' ) from dual;
+
+-- Tamanho do schema
+set linesize 150
+set pagesize 5000
+col owner for a15
+col segment_name for a30
+col segment_type for a20
+col tablespace_name for a30
+clear breaks
+clear computes
+compute sum of SIZE_IN_GB on report
+select owner,sum(bytes)/1024/1024/1024"SIZE_IN_GB" from dba_segments group by owner order by owner;
