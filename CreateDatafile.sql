23b6112e4744c7de5c514a531f959e954d5844f4
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-25 15:42:11 -0300

    Update CreateDatafile.sql

--- CreateDatafile.sql
+++ CreateDatafile.sql
@@ -7,3 +7,7 @@ ADD DATAFILE '+DG_DATA_VIA/viarac/datafile/consinco_dat79.dbf' size 31744M;
 ALTER TABLESPACE CONSINCO_DAT
 ADD DATAFILE '+DG_DATA_VIA/viarac/datafile/consinco_dat80.dbf' size 31744M AUTOEXTEND ON 
 NEXT 1024K MAXSIZE 32256M ;
+
+alter database
+datafile '+DATA/rms01_gru188/datafile/cpdinter_d_2.dbf'
+resize 31744M;
