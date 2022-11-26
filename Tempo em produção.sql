ee802552885bb7c80b6013a3f141e91e35a2f1c6
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-26 10:33:09 -0300

    Update Tempo em produção.sql

--- Tempo em produção.sql
+++ Tempo em produção.sql
@@ -1,1 +1,5 @@
 select 'Hostname : ' || host_name ,'Instance Name : ' || instance_name ,'Started At : ' || to_char(startup_time,'DD-MON-YYYY HH24:MI:SS') stime ,'Uptime : ' || floor(sysdate - startup_time) || ' days(s) ' || trunc( 24*((sysdate-startup_time) - trunc(sysdate-startup_time))) || ' hour(s) ' || mod(trunc(1440*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||' minute(s) ' || mod(trunc(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||' seconds' uptime from sys.v_$instance;
+
+SELECT database_name, 
+TO_CHAR(logon_time, 'DD-MM-YYYY HH24:MI:SS') 
+FROM v$session WHERE program LIKE '%PMON%';
