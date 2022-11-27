a7a6b8dced044fa0a096a03394d9dae4fd58c21b
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-27 00:51:08 -0300

    Update CheckArchiveLogMode.sql

--- CheckArchiveLogMode.sql
+++ CheckArchiveLogMode.sql
@@ -17,3 +17,6 @@ alter database open;
 select log_mode from v$database;
 
 archive log list
+
+-- ALTERAÇÃO DE DESTINO DE GERAÇÃO DOS ARCHIVES
+ALTER SYSTEM SET log_archive_dest_1='LOCATION=C:\oracle_19c\WINDOWS.X64_193000_db_home\rdbms\archive_dir_01';
