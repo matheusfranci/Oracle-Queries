fd5c14b01a31e52032efb6e37ca3f2949469b1f3
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-11-29 09:56:44 -0300

    Update CheckArchiveLogMode.sql

--- CheckArchiveLogMode.sql
+++ CheckArchiveLogMode.sql
@@ -19,4 +19,12 @@ select log_mode from v$database;
 archive log list
 
 -- ALTERAÇÃO DE DESTINO DE GERAÇÃO DOS ARCHIVES
-ALTER SYSTEM SET log_archive_dest_1='LOCATION=C:\oracle_19c\WINDOWS.X64_193000_db_home\rdbms\archive_dir_01';
+
+ALTER SYSTEM SET log_archive_dest_2='LOCATION=C:\Oracle\Archives\Dest2';
+ALTER SYSTEM SET log_archive_dest_3='LOCATION=C:\Oracle\Archives\Dest3';
+ALTER SYSTEM SET log_archive_dest_4='LOCATION=C:\Oracle\Archives\Dest4';
+ALTER SYSTEM SET log_archive_dest_5='LOCATION=C:\Oracle\Archives\Dest5';
+ALTER SYSTEM SET LOG_ARCHIVE_DEST_2='LOCATION=C:\Oracle\Archives\Dest2 MANDATORY ALTERNATE=LOG_ARCHIVE_DEST_3'
+
+-- Consultar os log_archive_dest
+select dest_name,status,destination from V$ARCHIVE_DEST;
