d4ad3d831bfdbc032b09a429b4d3064c2d3c1edb
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-12-06 10:37:32 -0300

    Update Lock query.sql

--- Lock query.sql
+++ Lock query.sql
@@ -1,3 +1,4 @@
+-- Single Instance
 SET LINESIZE 500
 SET PAGESIZE 1000
 COLUMN username FORMAT A15
@@ -16,3 +17,20 @@ TO_CHAR(s.logon_Time,'DD-MON-YYYY HH24:MI:SS') AS logon_time
 FROM v$session s
 CONNECT BY PRIOR s.sid = s.blocking_session
 START WITH s.blocking_session IS NULL;
+
+-- Oracle RAC
+SET LINESIZE 100
+COLUMN spid FORMAT A10
+COLUMN username FORMAT A10
+COLUMN program FORMAT A45
+
+SELECT s.inst_id,
+       s.sid,
+       s.serial#,
+       --s.sql_id,
+       p.spid,
+       s.username,
+       s.program
+FROM   gv$session s
+       JOIN gv$process p ON p.addr = s.paddr AND p.inst_id = s.inst_id
+WHERE  s.type != 'BACKGROUND';
