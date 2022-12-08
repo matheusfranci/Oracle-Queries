9545ceb64f4fc0e551e5bde0436d94df2f935fe4
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-12-08 09:54:45 -0300

    Update CheckArchiveLogMode.sql

--- CheckArchiveLogMode.sql
+++ CheckArchiveLogMode.sql
@@ -31,3 +31,26 @@ select dest_name,status,destination from V$ARCHIVE_DEST;
 
 -- Verificando o log archive format
  select name, value from v$spparameter where name in ('log_archive_dest_2','log_archive_format');
+
+-- Média de produção de gbs de archives
+SELECT A.*, 
+Round(A.Count#*B.AVG#/1024/1024/1024) Daily_Avg_gb 
+FROM 
+(SELECT 
+To_Char(First_Time,'YYYY-MM-DD') DAY, 
+Count(1) Count#, 
+Min(RECID) Min#, 
+Max(RECID) Max# 
+FROM v$log_history 
+GROUP 
+BY To_Char(First_Time,'YYYY-MM-DD') 
+ORDER 
+BY 1 
+) A, 
+(SELECT 
+Avg(BYTES) AVG#, 
+Count(1) Count#, 
+Max(BYTES) Max_Bytes, 
+Min(BYTES) Min_Bytes 
+FROM 
+v$log ) B;
