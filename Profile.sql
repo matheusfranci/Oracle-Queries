4081edb1668199f8dd9e17770ba57bdb32f75eb6
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-12-23 00:48:29 -0300

    Update Profile.sql

--- Profile.sql
+++ Profile.sql
@@ -4,4 +4,30 @@ Em ambientes multitentant, diferentes profiles podem ser associados a um usuári
 Quando um usuário comum se conecta em um PDB, um profile no qual ele está associado vai aplicar-se dependendo se está
 relacionado a configurações de senha ou de recursos.
 
+SESSIONS_PER_USER: especifica o número de sessões concorrentes para o qual você quer limitar o usuário.
+Ø CPU_PER_SESSION: especifica o tempo limite de CPU para uma sessão, expressada em centésimos de
+segundos.
+Ø CPU_PER_CALL: especifica o tempo limite para uma chamada(um parse, execute ou fetch), expressado em
+centésimos de segundos.
+Ø CONNECT_TIME: especifica o tempo total decorrido para uma sessão, expressado em minutos.
+Ø IDLE_TIME: especifica o período permitido de tempo inativo contínuo durante uma sessão, expressada em
+minutos. Queries longas e outras operações não estão sujeitas a este limite. Quando você configura idle
+timeout de X minutos, note que a sessão levará alguns minutos adicionais para ser finalizada. No lado cliente,
+da aplicação, uma mensagem de erro aparece somente na próxima vez, quando o client inativo tenta emitir
+um novo comando.
+Profile
+Parâmetros de Recursos
+Ø LOGICAL_READS_PER_SESSION: especifica o número permitido de data blocks lidos em uma sessão, incluindo
+blocos lidos da memória e do disco.
+Ø LOGICAL_READS_PER_CALL: especifica o número permitido de data blocks lidos para uma chamada de
+processo a um comando SQL(um parse, execute ou fetch).
+Ø PRIVATE_SGA: especifica o montante de espaço privado uma sessão pode alocar na shared pool da System
+Global Area(SGA). Este item aplica-se somente se você estiver utilizando a arquitetura shared server. O espaço
+privado para uma sessão na SGA inclui áreas privadas de SQL e PL/SQL, mas não para áreas compartilhadas de
+SQL e PL/SQL.
+Ø COMPOSITE_LIMIT: especifica o custo total de recurso para uma sessão, expressado em unidades de serviço.
+Oracle Database calcula as unidades totais de serviço como uma soma ponderada de CPU_PER_SESSION,
+CONNECT_TIME, LOGICAL_READS_PER_SESSION e PRIVATE_SGA.*/
+
+
 select * from v$instance;
