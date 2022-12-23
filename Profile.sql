3312c858c12fa8984edb2b87c94e80d9c4afc0d1
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2022-12-23 00:49:24 -0300

    Update Profile.sql

--- Profile.sql
+++ Profile.sql
@@ -1,4 +1,4 @@
-*/O profile é um recurso que possui a capacidade de limitar a utilização de alguns recursos do banco de dados. Você associa um
+/*O profile é um recurso que possui a capacidade de limitar a utilização de alguns recursos do banco de dados. Você associa um
 profile a um usuário e desta forma garante que ele não ultrapasse estes limites.
 Em ambientes multitentant, diferentes profiles podem ser associados a um usuário comum em um container root ou PDB.
 Quando um usuário comum se conecta em um PDB, um profile no qual ele está associado vai aplicar-se dependendo se está
