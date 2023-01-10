68855ff9c884dadd19991d8b7d30b793cc838b5d
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-01-10 16:01:59 -0300

    Update Gera script mata lock.sql

--- Gera script mata lock.sql
+++ Gera script mata lock.sql
@@ -3,3 +3,10 @@ FROM v$session s
 WHERE s.status = 'INACTIVE'
 CONNECT BY PRIOR s.sid = s.blocking_session
 START WITH s.blocking_session IS NULL;
+
+
+--RAC
+ select 'alter system kill session '''||v$session.sid||','||v$session.serial#||',@'||gv$session.inst_id||'''IMMEDIATE;' as script from gv$session, v$session 
+WHERE v$session.status = 'INACTIVE'
+CONNECT BY PRIOR v$session.sid = v$session.blocking_session
+START WITH v$session.blocking_session IS NULL;
