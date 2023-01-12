3c13330686ab6302e5f9fa22244571a775eeed63
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-01-11 21:39:22 -0300

    Update DataPump Progress.sql

--- DataPump Progress.sql
+++ DataPump Progress.sql
@@ -2,7 +2,8 @@ TTITLE LEFT '% Completed. Aggregate is the overall progress:'
 SET LINE 132
 SELECT opname, TO_CHAR(SYSDATE, 'DD/MM/YYYY HH24:MI:SS') AS "Time", round(sofar/totalwork*100) "% Complete"
 FROM gv$session_longops
-WHERE opname LIKE 'RMAN%'
+WHERE opname LIKE '%IMPORT%'
+OR opname LIKE '%EXPORT%'
 AND totalwork != 0
 AND sofar <> totalwork
 ORDER BY 1;
