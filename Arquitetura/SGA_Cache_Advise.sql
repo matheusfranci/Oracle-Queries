e1a11e9227bef8c175c1949dab8cffaaba193163
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-01-19 21:59:44 -0300

    Update and rename SGA Cache Advise.sql to Arquitetura/SGA_Cache_Advise.sql

--- Arquitetura/SGA_Cache_Advise.sql
+++ Arquitetura/SGA_Cache_Advise.sql
@@ -1,27 +1,14 @@
 select
-
 size_for_estimate ,
-
 buffers_for_estimate ,
-
 estd_physical_read_factor ,
-
 estd_physical_reads 
-
 from
-
 v$db_cache_advice
-
 where
-
 name = 'DEFAULT'
-
 and
-
 block_size = (SELECT value FROM V$PARAMETER
-
 WHERE name = 'db_block_size')
-
 and
-
 advice_status = 'ON';
