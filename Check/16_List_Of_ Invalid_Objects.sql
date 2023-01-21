fda127ee12aa7ca5cdd08d548fe85d4b6098f84d
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-01-21 10:58:37 -0300

    Update 16_List_Of_ Invalid_Objects.sql

--- Check/16_List_Of_ Invalid_Objects.sql
+++ Check/16_List_Of_ Invalid_Objects.sql
@@ -1,6 +1,8 @@
 select owner "Owner",
 object_name "Objeto",
-status "Estado"
+Object_type "Tipo",
+status "Estado",
+created "Criado em"
 from dba_objects
 where lower(status) != 'valid'
 order by owner, object_name;
