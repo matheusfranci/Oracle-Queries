34f67deee6d005faa2d0f67a3b573327c24278d4
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-02-06 00:02:56 -0300

    Update 16_Parfile_Datapump.sql

--- Procedimento/16_Parfile_Datapump.sql
+++ Procedimento/16_Parfile_Datapump.sql
@@ -8,7 +8,32 @@ expdp system/2022@ecoapp parfile=C:\oracle_19c\parameterfile.par logfile=export.
 -- Atachar no job e verificar os status
 expdp system/2022@ecoapp attach=EXPORT
 procedimento usuário/senha@instância attach=nomedojob
+Após atachar para verificar o status basta escrever status na linha de comando
 
+/*
+Export> status
+
+Job: EXPORT
+  OperaþÒo: EXPORT
+  Modo: FULL
+  Estado: EXECUTING
+  Bytes Processados: 0
+  Paralelismo Atual: 1
+  Contagem de Erros do Job: 0
+  PulsaþÒo do job: 6
+  Arquivo de Dump: C:\ORACLE_19C\EXPORT_ECOAPP.DMP
+    bytes gravados: 4.096
+
+Worker 1 Status:
+  ID da InstÔncia: 1
+  Instance name: orcl
+  Nome do host: MATHEUSHO
+  Horßrio de inÝcio do objeto: Quarta-Feira, 00 Sßb, 0000 0:00:00
+  Status do objeto em: Segunda-Feira, 06 Fevereiro, 2023
+  Nome do Processo: DW00
+  Estado: EXECUTING
+*/
+-- Procedimento para aumentar a streams_pool que auxilia na velocidade da importação
 select 'ALTER SYSTEM SET STREAMS_POOL_SIZE='||(max(to_number(trim(c.ksppstvl)))+67108864)||' SCOPE=SPFILE;' 
 from sys.x$ksppi a, sys.x$ksppcv b, sys.x$ksppsv c 
 where a.indx = b.indx 
@@ -21,5 +46,8 @@ To Change the STREAMS_POOL_SIZE:
 SQL> ALTER SYSTEM SET STREAMS_POOL_SIZE=100M SCOPE=both;
 System altered.
 
+-- Necessário rebootar a instância
 shut immediate;
 startup;
+
+
