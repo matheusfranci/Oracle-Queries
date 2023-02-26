49ffe20b8e54aaddc7d98ab9eaa356f0bcc958e4
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-02-26 02:10:34 -0300

    Update 25_CoalesceIndex.sql

--- Procedimento/25_CoalesceIndex.sql
+++ Procedimento/25_CoalesceIndex.sql
@@ -1,5 +1,6 @@
-*/De um modo geral, ao invés de um REBUILD, prefira executar o comando ALTER INDEX ... COALESCE.
+/*De um modo geral, ao invés de um REBUILD, prefira executar o comando ALTER INDEX ... COALESCE.
 pois ele não tem a mesma sobrecarga do REBUILD, podendo deste modo, ser executado em modo online (com os sistemas em execução).
 Apesar do COALESCE não fazer uma reestruturação completa dos índices (como o REBUILD faz), ele faz uma desfragmentação combinando os dados dos blocos das folhas,
-liberando desse modo, blocos que poderão ser usados para novas entradas nos índices, prevenindo-os de crescerem demasiadamente./* 
-alter index test_idx1 coalesce;
+liberando desse modo, blocos que poderão ser usados para novas entradas nos índices, prevenindo-os de crescerem demasiadamente.*/
+
+ALTER INDEX test_idx1 COALESCE;
