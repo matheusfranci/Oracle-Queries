fb0f0a557faf7bd23db1ab39d82aa9ba8b128f95
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-01 15:40:57 -0300

    Update 03_AutoExtendOn.sql

--- Generator/03_AutoExtendOn.sql
+++ Generator/03_AutoExtendOn.sql
@@ -1,1 +1,3 @@
 select 'alter database datafile '|| file_name|| ' '|| ' autoextend on maxsize unlimited;'from dba_data_files where autoextensible='NO';
+
+select 'ALTER DATABASE DATAFILE "'|| file_name|| '" '|| 'AUTOEXTEND ON MAXSIZE UNLIMITED;'from dba_data_files where autoextensible='NO';
