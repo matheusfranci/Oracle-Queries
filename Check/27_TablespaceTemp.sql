2da63cc01e17ea1231bb8721310e3d9811fabad8
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-01 10:15:26 -0300

    Update 27_TablespaceTemp.sql

--- Check/27_TablespaceTemp.sql
+++ Check/27_TablespaceTemp.sql
@@ -14,6 +14,17 @@ where tablespace_name = dba_tablespaces.tablespace_name) TOTAL
 from dba_tablespaces
 where contents = 'TEMPORARY') a;
 
+-- Check autoexntend on nos datafiles da temp
+column file_name format a30
+select file_name, tablespace_name, autoextensible
+from dba_temp_files;
+]-- Check tempfile
+select tablespace_name as "Tablespace",
+file_name as "Tempfile",
+bytes/1024/1024 as "Tamanho",
+maxbytes/1024/1024 as "Estende até"
+from dba_temp_files;
+
 ----CRIAÇÃO DE TEMPORARY
 ALTER TABLESPACE TEMP ADD TEMPFILE '+DATA' SIZE 30720M AUTOEXTEND ON;
 ALTER TABLESPACE TEMP ADD TEMPFILE '+DATA' SIZE 30720M AUTOEXTEND ON;
