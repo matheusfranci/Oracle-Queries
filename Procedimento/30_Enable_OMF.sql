53c653abbe20c71ccc821685ee58c1b4f38f4f89
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-04 02:43:20 -0300

    Update 30_Enable_OMF.sql

--- Procedimento/30_Enable_OMF.sql
+++ Procedimento/30_Enable_OMF.sql
@@ -10,6 +10,7 @@ alter system set db_recovery_file_dest='/u02/fast_recovery_area' scope=both;
 
 alter system set db_recovery_file_dest_size=10G scope=both;
 
+-- db_create_online_log_dest são parâmetros utilizados para multiplexar os redos
 alter system set DB_CREATE_ONLINE_LOG_DEST_1 ='/u02/oradata' scope=both;
 
 alter system set DB_CREATE_ONLINE_LOG_DEST_2 ='/u03/oradata' scope=both;
