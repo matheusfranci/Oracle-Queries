aa273c7aa442640d020338145205f82be168848b
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-04 02:41:51 -0300

    Create 30_Enable_OMF.sql

---
+++ Procedimento/30_Enable_OMF.sql
@@ -0,0 +1,17 @@
+show parameter db_create_file_dest
+
+show parameter db_create_online_log
+
+show parameter db_recovery_file_dest
+
+alter system set db_create_file_dest='/u02/oradata' scope=both;
+
+alter system set db_recovery_file_dest='/u02/fast_recovery_area' scope=both;
+
+alter system set db_recovery_file_dest_size=10G scope=both;
+
+alter system set DB_CREATE_ONLINE_LOG_DEST_1 ='/u02/oradata' scope=both;
+
+alter system set DB_CREATE_ONLINE_LOG_DEST_2 ='/u03/oradata' scope=both;
+
+alter system set LOG_ARCHIVE_DEST_1 ='LOCATION=USE_DB_RECOVERY_FILE_DEST' scope=both;
