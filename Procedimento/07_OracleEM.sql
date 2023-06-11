0bfc6a2f33089474441041a252c957ce581bb77c
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-06-11 08:50:20 -0300

    Update 07_OracleEM.sql

--- Procedimento/07_OracleEM.sql
+++ Procedimento/07_OracleEM.sql
@@ -15,3 +15,11 @@ lsnrctl status
 https://172.16.9.110:5500/em/
 
 -- Entre com a senha de sys configurada na instalação
+
+
+Note:In accordance with industry standards, Oracle is deprecating Flash-based Oracle Enterprise Manager Express (Oracle EM Express). Starting with Oracle Database 19c, Oracle EM Express, the default management option for Oracle Database, is based on Java JET technology. In this initial release, there are some options available in Flash-based Oracle EM Express that are not available in the JET version. If necessary, use the following command to revert to Flash Oracle EM Express:
+SQL> @?/rdbms/admin/execemx emx
+
+
+To return to JET Oracle EM Express, use the following command:
+SQL> @?/rdbms/admin/execemx omx
