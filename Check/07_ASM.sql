8c5826d709372f2ed0dfb74ed2af3f8da3c3e4f2
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-11-27 21:25:14 -0300

    Update 07_ASM.sql

--- Check/07_ASM.sql
+++ Check/07_ASM.sql
@@ -1,5 +1,6 @@
-SELECT name, type, total_mb, free_mb/1024 free_gb, required_mirror_free_mb, STATE,
-usable_file_mb FROM V$ASM_DISKGROUP;
+SELECT name, free_mb, total_mb, free_mb/total_mb*100 as percentage 
+     FROM v$asm_diskgroup;
+
 
 -- SCP dentro do asm
 -- Precisa ser feito dentro do usuário GRID
