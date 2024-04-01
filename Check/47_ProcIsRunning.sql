5e724fe0b374dae2ce67e496053761358b384855
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-04-01 20:34:15 -0300

    Create 47_ProcIsRunning.sql

---
+++ Check/47_ProcIsRunning.sql
@@ -0,0 +1,11 @@
+select 'CALLED PLSQL', vs.username, d_o.OWNER, d_o.object_name -- whatever info you need
+  from dba_objects d_o
+       inner join
+       v$session vs
+          on d_o.object_id = vs.plsql_entry_object_id
+union all
+select 'CURRENT PLSQL', vs.username, d_o.OWNER, d_o.object_name
+  from dba_objects d_o
+       inner join
+       v$session vs
+          on d_o.object_id = vs.plsql_object_id
