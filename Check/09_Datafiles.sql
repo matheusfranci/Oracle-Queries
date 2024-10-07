c9c34e9f4cf456b202c63e3caffbccdafb85d059
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-10-07 16:04:29 -0300

    Update 09_Datafiles.sql

--- Check/09_Datafiles.sql
+++ Check/09_Datafiles.sql
@@ -1,10 +1,13 @@
-col tablespace_name format a16;
-col file_name format a36;
-select file_name,
-tablespace_name,
-ROUND(bytes/1024/1024) AS currentbytesmb,
-ROUND(maxbytes/1024/1024) as mbmaxsize from dba_data_files
-order by tablespace_name, file_name;
+SET LINESIZE 200
+COL file_name FORMAT A100
+COL tablespace_name FORMAT A16
+
+SELECT file_name,
+       tablespace_name,
+       ROUND(bytes/1024/1024) AS currentbytesmb,
+       ROUND(maxbytes/1024/1024) AS mbmaxsize
+FROM dba_data_files
+ORDER BY tablespace_name, file_name;
 
 
 -- Check data de criação do datafile
