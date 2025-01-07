1fb8184a3cdf6ec1e3592fafcf898fbe2e269aa3
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2025-01-07 19:39:59 -0300

    Update RestoreArchiveLog.sql

--- Check/RestoreArchiveLog.sql
+++ Check/RestoreArchiveLog.sql
@@ -1,5 +1,5 @@
 set lines 9999
-set paegs 9999
+set pages 9999
 SELECT 'restore archivelog from logseq  ' ||To_Char(al.sequence# -1)||  ' until logseq ' ||l.SEQUENCE#|| ' thread ' ||l.THREAD#|| ';'
   FROM v$archived_log al, v$log L
     WHERE 20290265853035 BETWEEN AL.first_change# AND AL.next_change#
