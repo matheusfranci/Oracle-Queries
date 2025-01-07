e3f6cf4ade653b7aafb4de1fff297133636f0692
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2025-01-07 11:40:43 -0300

    Update 07_ASM.sql

--- Check/07_ASM.sql
+++ Check/07_ASM.sql
@@ -5,6 +5,16 @@ SELECT
     ROUND(free_mb/total_mb*100, 2) as "Livre%"
 FROM v$asm_diskgroup;
 
+-- Melhorada
+SELECT 
+(SELECT name FROM V$DATABASE) AS "Dbname",
+name as "Diskgroup",
+ROUND(total_mb / 1024) as "Total_ASM(GB)",
+ROUND(free_mb / 1024) as "Free_ASM(GB)",
+ROUND(free_mb/total_mb*100, 2) as "Perc_Livre",
+ROUND(100 - (free_mb/total_mb*100), 2) as "Perc_Ocup"
+FROM v$asm_diskgroup
+
 -- SCP dentro do asm
 -- Precisa ser feito dentro do usuário GRID
 ASMCMD> cp /u01/app/oracle/rm/datapump/EXPDP_RM.dmp +DATA/dump/EXPDP_RM.dmp
