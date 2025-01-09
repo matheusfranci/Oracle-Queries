502cad1b53a0920ee1ed28c509c65aa6c9e6b83e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2025-01-09 15:59:59 -0300

    Update 07_ASM.sql

--- Check/07_ASM.sql
+++ Check/07_ASM.sql
@@ -15,6 +15,24 @@ ROUND(free_mb/total_mb*100, 2) as "Perc_Livre",
 ROUND(100 - (free_mb/total_mb*100), 2) as "Perc_Ocup"
 FROM v$asm_diskgroup
 
+-- Validando um dgg especifico
+SELECT
+G.NAME AS "Disk Group",
+D.NAME AS "Discos",
+ROUND(D.TOTAL_MB / 1024, 2) AS SIZE_GB
+FROM V$ASM_DISK D
+INNER JOIN V$ASM_DISKGROUP G
+ON
+D.GROUP_NUMBER= G.GROUP_NUMBER
+WHERE G.NAME = 'SAUDEP_DATA1'
+order by D.NAME
+
+-- Adicionando:
+-- ADD ASM SAMPLE
+ALTER DISKGROUP SAUDEP_DATA1 ADD DISK
+'ORCL:SAUDEP_DATA1_D13' NAME SAUDEP_DATA1_D13,
+'ORCL:SAUDEP_DATA1_D14' NAME SAUDEP_DATA1_D14 REBALANCE Power 4;
+
 -- SCP dentro do asm
 -- Precisa ser feito dentro do usuário GRID
 ASMCMD> cp /u01/app/oracle/rm/datapump/EXPDP_RM.dmp +DATA/dump/EXPDP_RM.dmp
